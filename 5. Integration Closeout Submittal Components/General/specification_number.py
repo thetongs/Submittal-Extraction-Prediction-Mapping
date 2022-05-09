2f3bc07a4892cd0fa6853ceae8fc18e6e53c9ec9
Tongs <62232130+thetongs@users.noreply.github.com>
2022-05-09 09:54:46 +0530

    Delete 5. Integration Closeout Submittal Components directory

--- 5. Integration Closeout Submittal Components/General/specification_number.py
+++
@@ -1,7 +0,0 @@
-import re 
-
-def get_specification_number(str_data):
-
-    regex=r'SECTION\s*\d{2}\s*\d{2}\s*\d{2}[i]?[.0-9A-Za-z]*'
-    spec_num=re.search("\s*\d{2}\s*\d{2}\s*\d{2}[i]?[.0-9A-Za-z]*",re.search(regex,str_data,re.IGNORECASE).group()).group()
-    return spec_num.strip()
